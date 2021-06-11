be3c511c69673ab828fd41aee95c43c3423c1a31
Muhammad Usman Shahzad <usmanshahzad641@gmail.com>
2021-06-11 08:24:16 +0500

    Update index.js

--- resolvers/index.js
+++ resolvers/index.js
@@ -833,29 +833,29 @@ module.exports = {
         const { createReadStream, filename, mimetype, encoding } =
           await args.file;
         _filename = Math.random() * (9999999 - 1) + 1 + "-" + filename;
-        // await new Promise((res) =>
-        //   createReadStream()
-        //     .pipe(
-        //       createWriteStream(
-        //         path.join(__dirname + "./../uploads", _filename)
-        //       )
-        //     )
-        //     .on("close", res)
-        // );
-        var readStream = createReadStream(
-          path.join(__dirname + "./../uploads", _filename)
+        await new Promise((res) =>
+          createReadStream()
+            .pipe(
+              createWriteStream(
+                path.join(__dirname + "./../uploads", _filename)
+              )
+            )
+            .on("close", res)
         );
+        // var readStream = createReadStream(
+        //   path.join(__dirname + "./../uploads", _filename)
+        // );
 
-        // This will wait until we know the readable stream is actually valid before piping
-        readStream.on("open", function () {
-          // This just pipes the read stream to the response object (which goes to the client)
-          readStream.pipe(res);
-        });
+        // // This will wait until we know the readable stream is actually valid before piping
+        // readStream.on("open", function () {
+        //   // This just pipes the read stream to the response object (which goes to the client)
+        //   readStream.pipe(res);
+        // });
 
-        // This catches any errors that happen while creating the readable stream (usually invalid names)
-        readStream.on("error", function (err) {
-          res.end(err);
-        });
+        // // This catches any errors that happen while creating the readable stream (usually invalid names)
+        // readStream.on("error", function (err) {
+        //   res.end(err);
+        // });
         console.log(path.join(__dirname, "./../uploads", _filename));
         let Data = await createComplain(
           context.userId,
